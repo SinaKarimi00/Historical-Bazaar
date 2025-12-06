4c6a4a1a0be7df80a3519d3a8ee779f6edbc7ec6
Sina Karimi <sinakarimi0938@gmail.com>
2025-12-06 17:45:18 +0330

    Upload bae 1

---
+++ Assets/Features/Camera/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Features.Camera.Scripts
+{
+    public class CameraFollow : MonoBehaviour
+    {
+        public Transform target;
+        public float smoothSpeed = 5f;
+        public Vector3 offset = new Vector3(0, 20, -20);
+
+        void LateUpdate()
+        {
+            if (target == null) return;
+
+            Vector3 desiredPosition = target.position + offset;
+            transform.position = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+
+            // If camera is orthographic and has fixed rotation, do NOT rotate here.
+        }
+    }
+}
---
+++ Assets/Features/Player/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Features.Player.Scripts
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] private InputActionReference actionReference;
+        [SerializeField] private float speed;
+
+        private void Update()
+        {
+            ControlMovement();
+        }
+
+        private void ControlMovement()
+        {
+            var input = actionReference.action.ReadValue<Vector2>();
+
+            var move = new Vector3(input.x, 0f, input.y);
+
+            move = move.normalized * (speed * Time.deltaTime);
+
+            transform.Translate(move, Space.World);
+        }
+    }
+}
